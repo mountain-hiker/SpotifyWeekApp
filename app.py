c97a1b087510d5da982f7788597cbd17a4c384f1
David Garner <11651761+mountain-hiker@users.noreply.github.com>
2022-07-29 16:44:38 -0500

    add bulma, post endpoint for songs new song

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request, redirect, url_for
 from models.models import db, Playlist, PlaylistItem
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -9,7 +11,7 @@ db.init_app(app)
 db.create_all(app=app)
 
 @app.route("/")
-def test():
+def home():
     playlists = Playlist.query.all()
     tracks = PlaylistItem.query.all()
 
@@ -26,5 +28,17 @@ def test():
         
     return render_template('home.html',playlistViewModelArray=playlistViewModelArray)
 
-app.run()
+@app.route('/add-song', methods = ['POST'])
+def addSong():
+    song = request.form['song']
+    playlistId = request.form['playlist-id']
+    print(song)
+    print(playlistId)
 
+    newItem = PlaylistItem(track_title=song,spotify_track_id=random.random(),playlist_id=playlistId)
+    db.session.add(newItem)
+    db.session.commit()
+    
+    return redirect('/')
+    
+app.run(debug=True)
---
+++ static/js/home.js
@@ -0,0 +1,19 @@
+//On load
+(function() {
+    // modal.classList.add('is-active')
+ })();
+
+
+ function addSong(id){
+    console.log("Show modal for playlist with id of: " + id)
+    const modal = document.getElementById('add-modal')
+    modal.classList.add('is-active')
+
+    const playlistIdInput = document.getElementById('playlist-id-input')
+    playlistIdInput.value = id
+ }
+
+ function closeModal(){
+    const modal = document.getElementById('add-modal')
+    modal.classList.remove('is-active')
+ }
